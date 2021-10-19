0d04a3f236e13316c3ab0d18ec383c097bb84be2
Bhargodev Arya <bhargodevarya@gmail.com>
2021-10-19 19:48:12 +0530

    Repo setup

---
+++ blog_api/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+
+from blog_api.views import health_check
+
+urlpatterns = [
+    path('health', health_check)
+]
